4c5fdbb8d8a9e6a839a04410ca18345c1dda24a1
Shamimayesmin <shamimayesmin144@gmail.com>
2024-01-01 21:18:27 +0600

    added ui component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
-
-
+"use client";
 const HomePage = () => {
-  return (
-    <div>
-      <h1 className="text-5xl">This is home route</h1>
-    </div>
-  );
+	console.log("from server");
+	return (
+		<div>
+			<button className="btn">Button</button>
+			<button className="btn btn-neutral">Neutral</button>
+			<button className="btn btn-primary">Primary</button>
+			<button className="btn btn-secondary">Secondary</button>
+			<button className="btn btn-accent">Accent</button>
+			<button className="btn btn-ghost">Ghost</button>
+			<button className="btn btn-link">Link</button>
+			<h1 className="text-5xl">This is home route</h1>
+		</div>
+	);
 };
 
 export default HomePage;
