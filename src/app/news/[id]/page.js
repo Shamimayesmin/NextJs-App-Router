bfa023eed521ba843e3845351c28116545f6ec10
Shamimayesmin <shamimayesmin144@gmail.com>
2024-01-13 21:19:58 +0600

    dynamic route

---
+++ src/app/news/[id]/page.js
@@ -0,0 +1,11 @@
+
+
+const newsId = ({params, searchParams}) => {
+    return (
+        <div>
+            <h2>This is dynamic page ..............</h2>
+        </div>
+    );
+};
+
+export default newsId;
